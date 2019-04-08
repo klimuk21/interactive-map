3782443dcde127f47159600f364b7659e9ccc1cf
Alex Klimuk <klimuk.alex21@gmail.com>
2019-04-09 01:02:18 +0300

    edit description

--- balloon_html.js
+++ balloon_html.js
@@ -16,15 +16,21 @@ function init() {
         balloonContentHeader: '<b>Помнік землякам</b><br>',
         balloonContentBody: '<img src="1.jpg" height="350" width="300"><br>' +
             '<span>У цэнтры вёскі Міжлессе. Ва ўшанаванне памяці<br/> 248 воінаў-аднавяскоўцаў, загінуўшых у гады<br/> Вялікай Айчыннай вайны. Пастаулены ў 1993 г.</span>',
-        balloonContentFooter: 'Информация предоставлена:',
+        //balloonContentFooter: 'Информация предоставлена:',
         hintContent: '<b>Помнік землякам</b>'
+    },
+    {
+        preset: 'islands#redStarIcon'
     }))
     .add(new ymaps.Placemark([52.35045308, 25.03502124], {
         balloonContentHeader: '<b>Помнік землякам</b><br>',
         balloonContentBody: '<img src="2.jpg" height="300" width="400"><br>' +
             '<span>У вёсцы Судзілавічы. Ва ўшанаванне памяці 31 воіна-аднавяскоўца і 24 ахвяр фашызму. Пастаўлены ў 1985 г.</span>',
-        balloonContentFooter: 'Информация предоставлена:',
+        //balloonContentFooter: 'Информация предоставлена:',
         hintContent: '<b>Помнік землякам</b>'
+    },
+    {
+        preset: 'islands#redStarIcon'
     }))
     
     myMap.geoObjects.add(collection);
